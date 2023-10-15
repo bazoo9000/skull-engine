b095f5e49622b6127171ae3d3e38c9b163cffebe
bazoo9000 <stefandragosonica@gmail.com>
2023-10-15 21:02:43 +0300

    Triangle!

--- Skull/src/Skull/Application.cpp
+++ Skull/src/Skull/Application.cpp
@@ -22,6 +22,34 @@ namespace Skull {
 
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
+
+		// MAKE A TRIANGLE!
+
+		glGenVertexArrays(1, &m_VertexArray);
+		glBindVertexArray(m_VertexArray);
+
+		glGenBuffers(1, &m_VertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+		// merge de la -1 la 1 pe toate axele! (sau va fi convertit acolo)
+		float vertices[3 * 3] = {
+			-0.5f, -0.5f, 0.0f, // A
+			0.5f, -0.5f, 0.0f,  // B
+			0.0f, 0.5f, 0.0f    // C
+		};
+
+		// traducere: ducem datele din vertices din CPU in GPU, STATIC_DRAW este desenat doar o data
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		// asta traduce ce am trimis mai sus in ceva ce intelege GPU-ul
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr); // (index, cate vertex-uri, ce tip is, isNormalized?, cati bytes sunt intre vertex-uri, pointerul sau offset-ul (default ii 0))
+	
+		glGenBuffers(1, &m_IndexBuffer); // sau ElementBuffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+		unsigned int indices[3] = { 0, 1, 2 }; // merge si short
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	}
 
 	Application::~Application() {
@@ -58,8 +86,11 @@ namespace Skull {
 	void Application::Run() {
 
 		while (m_Running) {
-			glClearColor(1, 1, 1, 1); // flashbang 
+			glClearColor(0.2f, 0.2f, 0.2f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			glBindVertexArray(m_VertexArray); // already binded, dar nu strica sal apelezi din nou
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr); // draw indices
 
 			for (Layer* layer : m_LayerStack) {
 				layer->OnUpdate();
